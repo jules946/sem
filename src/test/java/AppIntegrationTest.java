37924fb3e72c4e83c3f6db727e3020eedd6ea4a5
jules946 <jjmanowski@gmail.com>
2023-04-25 11:14:22 +0100

    adding integration tests

---
+++ src/test/java/AppIntegrationTest.java
@@ -0,0 +1,25 @@
+import com.napier.sem.*;
+
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+public class AppIntegrationTest {
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+    }
+
+    @Test
+    void testGetEmployee()
+    {
+        Employee emp = app.getEmployee(255530);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
+    }
+}
